477b945e866bf7bcbd2c4ec0751ebcf65e3fd109
Marcel Pratikto <marcelaapratikto@gmail.com>
2023-12-26 11:23:28 -0700

    moved big data to the bottom

--- portfolio.py
+++ portfolio.py
@@ -14,29 +14,17 @@ for i in range(3):
     st.text(" \n")
 
 #=======================================================================================================
-# DS460 - Big Data Programming
-#=======================================================================================================
-st.markdown("### DS 460 - Big Data Programming")
-
-"""
-[App Development Project](https://mp-bigdataprogramming.streamlit.app/)
-"""
-
-for i in range(3):
-    st.text(" \n")
-
-#=======================================================================================================
 # ECEN 260 - Microprocessor Based System Design
 #=======================================================================================================
 st.markdown("### ECEN 260 - Microprocessor Based System Design")
 
 st.video("https://youtu.be/7ErvWzjYcJ8")
 
-st.markdown("Lab report with schematics for the project:")
+"""
+Lab report with schematics for the project:
 
-st.markdown(
-    "[ECEN 260 Lab Report](https://github.com/MarcelPratikto/Portfolio/blob/f94f30bf986394d08b9b5e7970bc91bf8b820062/data/ECEN260-Microprocessor/ECEN_260_Final_Project_Report.pdf)"
-)
+[ECEN 260 Lab Report](https://github.com/MarcelPratikto/Portfolio/blob/f94f30bf986394d08b9b5e7970bc91bf8b820062/data/ECEN260-Microprocessor/ECEN_260_Final_Project_Report.pdf)
+"""
 
 for i in range(3):
     st.text(" \n")
@@ -47,6 +35,18 @@ for i in range(3):
 st.markdown("### CSE 310 - Applied Programming")
 
 st.video("https://youtu.be/vGSW8i1kw-0")
+
+for i in range(3):
+    st.text(" \n")
+
+#=======================================================================================================
+# DS460 - Big Data Programming
+#=======================================================================================================
+st.markdown("### DS 460 - Big Data Programming")
+
+"""
+[App Development Project](https://mp-bigdataprogramming.streamlit.app/)
+"""
 
 for i in range(3):
     st.text(" \n")
